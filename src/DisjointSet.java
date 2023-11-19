9b607eb66a029ba5d93799a24b4c51e4fc674f89
Peter Kurtz <peterjohnkurtz@gmail.com>
2023-11-18 23:23:18 -0700

    Implemented DisjointSet.

---
+++ src/DisjointSet.java
@@ -0,0 +1,36 @@
+public class DisjointSet {
+    private int[] set;
+
+    public DisjointSet(int size) {
+        set = new int[size];
+        for (int i = 0; i < size; i++){
+            set[i] = -1;
+        }
+    }
+
+    public int find(int node) {
+        if (set[node] < 0) {
+            return node;
+        }
+
+        int root = find(set[node]);
+        set[node] = root;
+        return root;
+    }
+
+    public void union(int node1, int node2) {
+        int root1 = find(node1);
+        int root2 = find(node2);
+
+        if (set[root2] < set[root1]) {
+            set[root2] += set[root1];
+            set[root1] = root2;
+        }
+
+        else {
+            set[root1] += set[root2];
+            set[root2] = root1;
+        }
+        System.out.println();
+    }
+}
