ceb764f65faa62d6640ff0518fc4f40f94feb89d
Peter Kurtz <peterjohnkurtz@gmail.com>
2023-11-18 23:25:57 -0700

    Added HexGame. Made Extra to put tested code.

--- src/Assignment6Driver.java
+++ src/Assignment6Driver.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 public class Assignment6Driver {
     public static void main(String[] args) {
 
-        DisjointSet test8 = new DisjointSet(8);
-        test8.union(1,4);
-        test8.union(0,3);
-        test8.union(0,1);
-        test8.union(7,6);
-        test8.union(2,4);
-        System.out.println();
         //testGame();
         //playGame("moves1.txt");
         //System.out.println();
---
+++ src/Extra.java
@@ -0,0 +1,10 @@
+/*
+    DisjointSet test8 = new DisjointSet(8);
+    test8.union(1,4);
+    test8.union(0,3);
+    test8.union(0,1);
+    test8.union(7,6);
+    test8.union(2,4);
+    System.out.println();
+
+*/
